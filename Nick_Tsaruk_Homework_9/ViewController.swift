0ca2feb899f90d5c0f017ffe9ae7eee6bcfc40d8
Nvtsaruk <nvtsaruk@gmail.com>
2023-07-02 16:07:29 +0300

    Initial Commit

---
+++ Nick_Tsaruk_Homework_9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nick_Tsaruk_Homework_9
+//
+//  Created by Tsaruk Nick on 2.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
